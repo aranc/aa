9a42e99093af862785b2a71bfd0ba2341c8ae408
Aran Carmon <aran.carmon@gmail.com>
2017-01-17 01:39:03 +0200

    more

--- t.py
+++ t.py
@@ -1,13 +1,19 @@
 import math
 from math import log, ceil, floor
 
+def my_floor_log(x):
+    return int(floor(log(x)/log(2)))
+
 def c1(b, k):
     c1 = 1 << b
     i = 0
-    for _ in range(int(floor(log(2*k)/log(2)))):
+    #for _ in range(my_floor_log(2*k)):
+    for tmp1 in range(my_floor_log(k)):
         i = i + 1
-        t = c1 << ((b + 1) * i)
+        t = c1
+        for tmp2 in range(b):
+            t = t << (1 << i)
         c1 = c1 | t
     return bin(c1)
 
-print [(i, c1(2,i).count('1')) for i in range(1,10)]
+print [(2**i, c1(2,2**i).count('1')) for i in range(1,10)]
