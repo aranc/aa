0839416c3a80b0959f6cc11c40f1564db58936cf
Aran Carmon <aran.carmon@gmail.com>
2017-01-17 01:44:41 +0200

    more

--- t.py
+++ t.py
@@ -7,13 +7,11 @@ def my_floor_log(x):
 def c1(b, k):
     c1 = 1 << b
     i = 0
-    #for _ in range(my_floor_log(2*k)):
+    #for tmp1 in range(my_floor_log(2*k)):
     for tmp1 in range(my_floor_log(k)):
-        i = i + 1
-        t = c1
-        for tmp2 in range(b):
-            t = t << (1 << i)
+        t = c1 << ((b+1) * (1 << i))
         c1 = c1 | t
+        i = i + 1
     return bin(c1)
 
 print [(2**i, c1(2,2**i).count('1')) for i in range(1,10)]
