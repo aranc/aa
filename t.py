3c7bf0a174b6d7f0ce80f87ca17dfdfe7caaea1b
Aran Carmon <aran.carmon@gmail.com>
2017-01-17 16:55:14 +0200

    more

--- t.py
+++ t.py
@@ -30,14 +30,45 @@ def my_bin(x, g):
     return "".join(reversed(T))
     
 
-if True:
+def try1():
     k=3
     b=4
     _sum = 0
     for i in [0,1,2]:
-        res = (c1(k,((b)<<i))>>(b-i))
+        res = (c1(k,((b+1)<<i)-1)>>(b-i))
         _sum += res
         #print i, bin(res), my_bin(res)
         print i, my_bin(res, b+1)
     #print bin(_sum)
 
+def try2():
+    k=3
+    b=4
+    _sum = 0
+    for i in [0,1,2]:
+        res = (c1(k,((b+1)<<i)-1)>>(b-i))
+        _sum += res
+        #print i, bin(res), my_bin(res)
+        print i, my_bin(res, b+1)
+    #print my_bin(_sum, b+1)
+
+#try2()
+
+def goal():
+    print "0001 0000 0001 0000 0001 0000 0001 0000"
+    print "0010 0010 0000 0000 0010 0010 0000 0000"
+    print "0100 0100 0100 0100 0000 0000 0000 0000"
+    print "0111 0110 0101 0100 0011 0010 0001 0000"
+#goal()
+
+def ct(k,b):
+    c1 = (1 << b) + 1
+    i = 0
+    for tmp1 in range(my_ceil_log(2*k)):
+    #for tmp1 in range(my_floor_log(k)):
+        t = c1 << ((b+1) << i) + 1
+        c1 = c1 + t
+        i = i + 1
+    #return bin(c1)
+    return (c1)
+print my_bin(ct(3, 4), 4+1)
